02dc47f7ed9bdd211c4ffdef1fd120f310be1611
jaytohe <antolbestg4@gmail.com>
2020-01-28 16:55:12 +0200

    Added nag errors. Added XSS protection.

--- login/error.php
+++ login/error.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
+$nag_error1="Username or password is wrong.";
+$nag_error2="Please double-check and try again.";
 if(isset($_GET['u'])) {
-$usr=$_GET['u'];
+$usrparam=rawurldecode($_GET['u']);
+$usrparam = stripcslashes(htmlspecialchars($usrparam));
+}
+if(isset($_GET['conn'])) {
+	$nag_error1="A MySQL error has occured.";
+	$na_error2="Please contact your administrator.";
 }
 include 'index.php';
-if(isset($_SESSION['err'])) { //Login error handling not implemented yet.
-}
 ?>
